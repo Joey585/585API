04bf6f6d33069e788a3e953284ec12a91fc88d4b
Joey585 <thelich06@protonmail.com>
2023-04-23 00:52:00 -0700

    Added SQL support
    yay

--- food.php
+++ food.php
@@ -4,9 +4,13 @@
 </head>
 <body>
     <?php
+        $uri = 'mongodb+srv://joey585:$Vapor7275$@cluster0.2dqvy.mongodb.net/?retryWrites=true&w=majority';
+
+
+
+
         $file_name = $_FILES["file"]["name"];
         $file_temp_location = $_FILES["file"]["tmp_name"];
-
         if(!$file_temp_location){
             echo "ERROR: No file selected";
             exit();
@@ -16,6 +20,28 @@
             echo "Thank you for uploading your beautiful food picture!";
         } else {
             echo "Error!";
+        }
+
+
+        try {
+            $conn = new mysqli("localhost:3306", "root", "", "585API");
+            $title = $_POST["title"];
+            $tags = str_replace(" ", ",", $_POST["tags"]);
+            $location = "food-pics/$file_name";
+            $views = 0;
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $query = "INSERT INTO FoodPics (title, tags, location, views) VALUES (?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $query) or die(mysqli_error($conn));
+            $stmt->bind_param("ssss", $title, $tags, $location, $views);
+            $stmt->execute();
+            $stmt->close();
+
+        } catch (Exception $exception){
+            echo $exception;
         }
     ?>
 </body>
